bc70d405a6b82702dd94b23096894fbe13ac9638
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-18 19:52:33 +0900

    ServeHTTPメソッドを追加
    テンプレートを1回だけコンパイルし、コンパイルされたテンプレートの参照を保持

--- chat/main.go
+++ chat/main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 	"sync"
 )
 
@@ -11,6 +12,13 @@ type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
 }
 
 func main() {
