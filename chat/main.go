50affdc6116cfb38dfbcb41563727689b72cf3d1
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-18 19:49:23 +0900

    Add templateHandler struct

--- chat/main.go
+++ chat/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
