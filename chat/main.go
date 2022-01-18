e2c263408da83768a987390e1972804d5e937c7e
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-18 19:38:44 +0900

    Add handle func to main func
    `go run main.go` returns html include hello, world.

--- chat/main.go
+++ chat/main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<p>Hello, world</p>
+			</html>
+		`))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
