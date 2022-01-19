acc09eb340adc3de4dcfa97c43473dbc6c3e5a7b
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-19 18:37:12 +0900

    Add run method to room.go

--- chat/room.go
+++ chat/room.go
@@ -6,3 +6,30 @@ type room struct {
 	leave   chan *client
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	// goroutineでバックグラウンドで実行される無限ループ
+	// アプリケーション内の他の処理をブロックすることはない
+
+	// join,leave,forwardのチャネルを監視して
+	// いずれかにメッセージが届くとcase節が実行される
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
