3ec9c6c8d8b2eb0532ff3fe69118465a4e9f9aa1
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-21 19:42:16 +0900

    Fix TestNew

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,9 +1,21 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnil")
+	} else {
+		tracer.Trace("こんにちは")
+		if buf.String() != "こんにちは" {
+			t.Errorf("%s is not expected", buf.String())
+		}
+	}
+
 	t.Error("まだテストしていない")
 }
