5fe953061b49e984e6a9f667eec55bf30c516a40
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-22 20:12:31 +0900

    Fix main to log

--- chat/main.go
+++ chat/main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/kenzo-takana/go_webapp/trace"
 )
 
 type templateHandler struct {
@@ -27,6 +30,7 @@ func main() {
 	flag.Parse()
 
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
