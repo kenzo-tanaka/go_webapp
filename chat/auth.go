7dcf2379a297605bd45b03f2eddff2773431b325
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-27 20:28:38 +0900

    Add case callback

--- chat/auth.go
+++ chat/auth.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
 
 type authHandler struct {
@@ -48,6 +49,30 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("GetBeginAuthURL error", loginUrl)
 		}
 		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("auth provider failed", provider)
+		}
+
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("authentiacation failed", provider, err)
+		}
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("get user failed", provider, err)
+		}
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+		w.Header()["Location"] = []string{"/chat"}
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
